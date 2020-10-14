1bf08eafe00a5e1fcdcad247a713bac1f1a6c346
zybk95 <abdulkadirzeybek95@gmail.com>
2020-10-14 10:52:25 +0300

    mailto

---
+++ docs/js/mailto.js
@@ -0,0 +1,24 @@
+var initSubject='',initBody='';
+ 
+
+function submitHandler(){
+    var to = "bilgi@hayteknoloji.com";
+    var name = nameText.value;
+    var email = emailText.value;
+    var tel = telText.value;
+    var subject = subText.value;
+
+    var body = ""+bodyText.value+'%0A%0A%0A';
+        body += "From："+nameText.value+'%0A';
+        body += "Email："+emailText.value+'%0A';
+        body += "Tel："+telText.value;
+
+    mailTo.href="mailto:"+to+"?subject="+subject+"&body="+body;
+    mailTo.click();
+}
+
+function init(){
+    subText.value=initSubject;
+    //nameText.value=initTo;
+    bodyText.value=initBody;
+}
